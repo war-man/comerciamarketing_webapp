a2571db85e76034462b23836fa89e207302ce83d
Francisco Velásquez <franvelasquez194@gmail.com>
2018-03-23 15:36:49 -0600

    web app conectada a azure y azure db, solo faltan detalles

--- comerciamarketing_webapp/Controllers/HomeController.cs
+++ comerciamarketing_webapp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using comerciamarketing_webapp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +7,31 @@ using System.Web.Mvc;
 
 namespace comerciamarketing_webapp.Controllers
 {
+    
     public class HomeController : Controller
     {
+        private dbComerciaEntities db = new dbComerciaEntities();
+        public ActionResult Main()
+        {
+
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
+
+                ViewBag.usuario = datosUsuario.usuario;
+                ViewBag.nomusuarioSAP = datosUsuario.NOM_clienteSAP;
+                ViewBag.tipomembresia = datosUsuario.Tipo_membresia.descripcion;
+
+
+                return View();
+            }
+            else {
+                return RedirectToAction("Index");
+            }
+
+                
+        }
         public ActionResult Index()
         {
             return View();
@@ -15,16 +39,68 @@ namespace comerciamarketing_webapp.Controllers
 
         public ActionResult Dashboard()
         {
-            ViewBag.Message = "Your application description page.";
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
 
-            return View();
+                ViewBag.usuario = datosUsuario.usuario;
+                ViewBag.nomusuarioSAP = datosUsuario.NOM_clienteSAP;
+
+                //Consultamos los recursos
+
+                var recursos = (from b in db.Recursos_usuario where (b.ID_usuario == ID && b.tipo_recurso == 1) select b).FirstOrDefault();
+
+                if (recursos != null)
+                {
+                    ViewBag.url = recursos.url;
+
+                    return View();
+                }
+                else {
+                    TempData["advertencia"] = "Aún no han asignado un recurso para mostrar.";
+                    return RedirectToAction("Main");
+                }
+
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult Iniciar_sesion(string usuariocorreo, string password)
         {
-            ViewBag.Message = "Your contact page.";
+            //Validamos del lado del cliente que ambos parametros no vengan vacios
+            try
+            {
+                var obj = (from c in db.Usuarios where (c.usuario == usuariocorreo && c.contrasena == password) select c).FirstOrDefault();
+                if (obj != null)
+                {
 
-            return View();
+                    Session["IDusuario"] = obj.ID_usuario.ToString();
+                    
+                    return RedirectToAction("Main");
+                }
+                else
+                {
+                    //Si ingreso mal la contraseña o el usuario no existe
+                    TempData["advertencia"] = "Correo o contraseña incorrecta.";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception exception)
+            {
+                TempData["error"] = "Ocurrió un error." + exception;
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Cerrar_sesion()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Index");
         }
     }
 }
