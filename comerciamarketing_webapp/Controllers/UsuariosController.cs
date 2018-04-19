1b63feae376a3978cdebda723bc66d0f6e34bfec
Francisco Velásquez <f.velasquez@limenainc.net>
2018-04-19 10:51:54 -0600

    Saved 04/19/2018

--- comerciamarketing_webapp/Controllers/UsuariosController.cs
+++ comerciamarketing_webapp/Controllers/UsuariosController.cs
@@ -95,8 +95,10 @@ namespace comerciamarketing_webapp.Controllers
                 var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
 
                 ViewBag.usuario = datosUsuario.correo;
-                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia, "ID_tipomembresia", "descripcion");
-                ViewBag.ID_rol = new SelectList(db.Roles, "ID_rol", "descripcion");
+                //excluimos el tipo de membresia con ID 6 ya que este es el asignado para DEMOS
+                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia.Where(c=> c.ID_tipomembresia != 6), "ID_tipomembresia", "descripcion");
+                //excluimos ademas los roles de Demos
+                ViewBag.ID_rol = new SelectList(db.Roles.Where(c => c.ID_rol != 6 && c.ID_rol != 7), "ID_rol", "descripcion");
                 ViewBag.ID_empresa = ID_empresa;
                 return View();
             }
@@ -160,8 +162,10 @@ namespace comerciamarketing_webapp.Controllers
                 {
                     return HttpNotFound();
                 }
-                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia, "ID_tipomembresia", "descripcion", usuarios.ID_tipomembresia);
-                ViewBag.ID_rol = new SelectList(db.Roles, "ID_rol", "descripcion", usuarios.ID_rol);
+                //excluimos el tipo de membresia con ID 6 ya que este es el asignado para DEMOS
+                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia.Where(c => c.ID_tipomembresia != 6), "ID_tipomembresia", "descripcion", usuarios.ID_tipomembresia);
+                //excluimos ademas los roles de Demos
+                ViewBag.ID_rol = new SelectList(db.Roles.Where(c => c.ID_rol != 6 && c.ID_rol != 7), "ID_rol", "descripcion", usuarios.ID_rol);
                 ViewBag.ID_empresa = new SelectList(db.Empresas, "ID_empresa", "nombre", usuarios.ID_empresa);
 
                 ViewBag.ID_empresaback = usuarios.ID_empresa;
@@ -352,7 +356,7 @@ namespace comerciamarketing_webapp.Controllers
                     var id_empresa = (from b in db.Empresas where (b.nombre == "SISTEMA") select b).FirstOrDefault();
             
 
-                return View(db.Usuarios.Where(c =>c.ID_empresa == id_empresa.ID_empresa).ToList());
+                return View(db.Usuarios.Where(c =>c.ID_empresa == id_empresa.ID_empresa && c.activo ==false && c.ID_rol==1).ToList());
 
 
         }
@@ -375,8 +379,10 @@ namespace comerciamarketing_webapp.Controllers
                 {
                     return HttpNotFound();
                 }
-                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia, "ID_tipomembresia", "descripcion", usuarios.ID_tipomembresia);
-                ViewBag.ID_rol = new SelectList(db.Roles, "ID_rol", "descripcion", usuarios.ID_rol);
+                //excluimos el tipo de membresia con ID 6 ya que este es el asignado para DEMOS
+                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia.Where(c => c.ID_tipomembresia != 6), "ID_tipomembresia", "descripcion", usuarios.ID_tipomembresia);
+                //excluimos ademas los roles de Demos
+                ViewBag.ID_rol = new SelectList(db.Roles.Where(c => c.ID_rol != 6 && c.ID_rol != 7), "ID_rol", "descripcion", usuarios.ID_rol);
                 ViewBag.ID_empresa = new SelectList(db.Empresas.Where(c=>c.nombre!="SISTEMA"), "ID_empresa", "nombre", usuarios.ID_empresa);
 
                 ViewBag.ID_empresaback = usuarios.ID_empresa;
@@ -497,7 +503,7 @@ namespace comerciamarketing_webapp.Controllers
 
         }
 
-        public ActionResult Login_history(int? id)
+        public ActionResult Login_history(int? id, string modulo)
         {
 
             int ID = Convert.ToInt32(Session["IDusuario"]);
@@ -508,13 +514,14 @@ namespace comerciamarketing_webapp.Controllers
             var usuarios = (from e in db.Usuarios where (e.ID_usuario == id) select e).FirstOrDefault();
             ViewBag.CorreoUsuario = usuarios.correo;
             ViewBag.ID_empresaback = usuarios.ID_empresa;
+            ViewBag.modulo = modulo;
 
             return View(db.historial_conexiones.Where(c => c.ID_usuario == id).OrderByDescending(c=>c.fecha_conexion).Take(20).ToList());
 
 
         }
 
-        public ActionResult Details_loginhistory(int? id)
+        public ActionResult Details_loginhistory(int? id, string modulo)
         {
 
             if (Session["IDusuario"] != null)
@@ -535,6 +542,7 @@ namespace comerciamarketing_webapp.Controllers
                 }
 
                 ViewBag.ID_usuarioback = historial.ID_usuario;
+                ViewBag.modulo = modulo;
                 return View(historial);
             }
             else
@@ -556,7 +564,7 @@ namespace comerciamarketing_webapp.Controllers
                 ViewBag.usuario = datosUsuario.correo;
 
 
-                var usuarios = db.Usuarios.Where(c => c.ID_empresa == 2 && c.ID_tipomembresia ==6 && c.ID_rol == 7).Include(u => u.Tipo_membresia).Include(u => u.Recursos_usuario).Include(u => u.Roles);
+                var usuarios = db.Usuarios.Where(c => c.ID_empresa == 2 && c.ID_tipomembresia ==6).Include(u => u.Tipo_membresia).Include(u => u.Recursos_usuario).Include(u => u.Roles);
 
                 foreach (var item in usuarios)
                 {
@@ -580,6 +588,221 @@ namespace comerciamarketing_webapp.Controllers
             else
             {
                 return RedirectToAction("Index", "Home", null);
+            }
+
+        }
+
+        // GET:
+        public ActionResult Details_demoU(int? id)
+        {
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
+
+                ViewBag.usuario = datosUsuario.correo;
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Usuarios usuarios = db.Usuarios.Find(id);
+                if (usuarios == null)
+                {
+                    return HttpNotFound();
+                }
+
+                
+                return View(usuarios);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+
+
+        }
+
+
+        // GET:
+        public ActionResult Create_demoU()
+        {
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
+
+                ViewBag.usuario = datosUsuario.correo;
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+
+        }
+
+        // POST:
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create_demoU([Bind(Include = "ID_usuario,correo,contrasena,ID_tipomembresia,ID_rol,fcreacion_usuario,activo,nombre,apellido,cargo,telefono,estados_influencia,ID_empresa")] Usuarios usuarios)
+        {
+            if (usuarios.cargo == null)
+            {
+                usuarios.cargo = "";
+            }
+            if (usuarios.telefono == null)
+            {
+                usuarios.telefono = "";
+            }
+            if (usuarios.estados_influencia == null)
+            {
+                usuarios.estados_influencia = "";
+            }
+            //predeterminados
+
+            usuarios.ID_empresa = 2; //empresa sistema
+            usuarios.fcreacion_usuario = DateTime.Now;
+            usuarios.ID_tipomembresia = 6;
+            usuarios.ID_rol = 7;
+
+            usuarios.activo = true;
+
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                db.SaveChanges();
+                TempData["exito"] = "User created successfully.";
+                return RedirectToAction("Demo_users", "Usuarios");
+            }
+            TempData["advertencia"] = "Something wrong happened, try again.";
+            return RedirectToAction("Demo_users", "Usuarios");
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit_demoU(int? id)
+        {
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
+
+                ViewBag.usuario = datosUsuario.correo;
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Usuarios usuarios = db.Usuarios.Find(id);
+                if (usuarios == null)
+                {
+                    return HttpNotFound();
+                }
+                ViewBag.ID_tipomembresia = new SelectList(db.Tipo_membresia, "ID_tipomembresia", "descripcion", usuarios.ID_tipomembresia);
+                ViewBag.ID_rol = new SelectList(db.Roles, "ID_rol", "descripcion", usuarios.ID_rol);
+                ViewBag.ID_empresa = new SelectList(db.Empresas, "ID_empresa", "nombre", usuarios.ID_empresa);
+
+
+                return View(usuarios);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit_demoU([Bind(Include = "ID_usuario,correo,contrasena,ID_tipomembresia,ID_rol,fcreacion_usuario,activo,nombre,apellido,cargo,telefono,estados_influencia,ID_empresa")] Usuarios usuarios)
+        {
+            if (usuarios.cargo == null)
+            {
+                usuarios.cargo = "";
+            }
+            if (usuarios.telefono == null)
+            {
+                usuarios.telefono = "";
+            }
+            if (usuarios.estados_influencia == null)
+            {
+                usuarios.estados_influencia = "";
+            }
+
+            //predeterminados
+
+            usuarios.ID_empresa = 2; //empresa sistema
+            usuarios.fcreacion_usuario = DateTime.Now;
+            usuarios.ID_tipomembresia = 6;
+            usuarios.ID_rol = 7;
+
+            usuarios.activo = true;
+
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["exito"] = "User saved successfully.";
+                return RedirectToAction("Demo_users", "Usuarios");
+            }
+            TempData["advertencia"] = "Something wrong happened, try again.";
+            return RedirectToAction("Demo_users", "Usuarios");
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete_demoU(int? id)
+        {
+            if (Session["IDusuario"] != null)
+            {
+                int ID = Convert.ToInt32(Session["IDusuario"]);
+                var datosUsuario = (from c in db.Usuarios where (c.ID_usuario == ID) select c).FirstOrDefault();
+
+                ViewBag.usuario = datosUsuario.correo;
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Usuarios usuarios = db.Usuarios.Find(id);
+                if (usuarios == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(usuarios);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete_demoU")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed_demoU(int id)
+        {
+
+            try
+            {
+                Usuarios usuarios = db.Usuarios.Find(id);
+                var id_back = usuarios.ID_empresa;
+                db.Usuarios.Remove(usuarios);
+                db.SaveChanges();
+                TempData["exito"] = "User deleted successfully.";
+                return RedirectToAction("Demo_users", "Usuarios");
+            }
+            catch (Exception ex)
+            {
+                Usuarios usuarios = db.Usuarios.Find(id);
+                var id_back = usuarios.ID_empresa;
+                TempData["error"] = "An error was handled.  " + ex.Message;
+                return RedirectToAction("Demo_users", "Usuarios");
             }
 
         }
